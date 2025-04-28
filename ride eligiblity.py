64ab0d0b2750a84926383368638e5f37ef7a7669
khushi yadav <khushigkp.yadav@gmail.com>
2025-04-28 22:48:50 +0530

    Update ride eligiblity.py

--- ride eligiblity.py
+++ ride eligiblity.py
@@ -1,3 +1,9 @@
+''' You are at an amusement park. The ride eligibility is based on height and age. Write a Python control flow code to check if a person can ride.
+If the person is under 12 years old, they must be at least 120 cm tall to ride.
+If the person is 12 to 18 years old, they can ride if they are at least 110 cm tall.
+If the person is older than 18, they can ride regardless of height.
+'''
+
 age = int(input("Enter the age: "))
 height = int(input("Enter the height: "))
 
