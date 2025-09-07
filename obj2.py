771c62f7833258d312be0df2f111d7393a25a577
khushi yadav <khushigkp.yadav@gmail.com>
2025-09-07 12:29:14 +0530

    Create obj2.py

---
+++ obj2.py
@@ -0,0 +1,8 @@
+# Make a class Laptop with a method specs() 
+# that prints "This is a laptop". Create an object and
+# call the method.
+class Laptop:
+    def specs(self): 
+      print("This is a laptop");
+l=Laptop()
+l.specs()
