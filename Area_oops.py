c53953bb2c0c186b8a6458d2e63e808885bb968e
khushi yadav <khushigkp.yadav@gmail.com>
2025-09-07 16:00:45 +0530

    Create Area_oops.py

---
+++ Area_oops.py
@@ -0,0 +1,25 @@
+#  Create a class Shape with method area().
+#   Then make two classes:
+#   - Rectangle with width and height
+#   - Circle with radius
+#   Both should override area() to give correct output. Call area() for both objects.
+class Shape:
+    def area(self):
+        print("area")
+class Rectangle(Shape):
+     def __init__(self, width , height):
+       self.width=width
+       self.height=height
+     def area(self):
+         return self.width *self.height
+class  Circle(Shape):
+      def __init__(self,radius):
+         self.radius=radius
+      def area(self):
+          return 2*3.14*self.radius*self.radius
+r=Rectangle(2,3)
+c=Circle(2)
+print(r.area())
+print(c.area())
+
+
